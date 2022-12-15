428cb75c8bc653b9a9c8f478b2798be9f50f8f90
Surawich Sittisarn <surawich.sit@gmail.com>
2022-12-16 01:01:15 +0700

    day12

--- src/day12.ts
+++ src/day12.ts
@@ -1,21 +1,46 @@
 import * as fs from "fs";
 
 const buffer = fs.readFileSync("./src/day12.txt");
-const fileContent = buffer.toString().trim().split("\n");
+const lines = buffer.toString().trim().split("\n");
 
-let startingPoint: number[] = [];
-const map = fileContent.map((line) => {
-    const arrayLine = line.split("");
-    arrayLine.map((char) => {
-        if (char == "S") {
-            startingPoint = [fileContent.indexOf(line), arrayLine.indexOf("S")];
-        }
-    });
-    return arrayLine.map((char) => {
-        return char.charCodeAt(0);
-    });
-});
+const terrain: number[] = [];
 
-console.log(startingPoint);
+for (let i = 0; i < lines.length; i++)
+    for (let j = 0; j < lines[i].length; j++)
+        terrain.push(lines[i][j].charCodeAt(0));
 
-let step = 0;
+const start = terrain.findIndex((i) => i === "S".charCodeAt(0));
+const end = terrain.findIndex((i) => i === "E".charCodeAt(0));
+
+terrain[start] = "a".charCodeAt(0);
+terrain[end] = "z".charCodeAt(0);
+
+const bfs = (start: number) => {
+    let queue = [[start, 0]];
+    const cache = new Set([start]);
+    while (queue.length) {
+        const tmpArr = queue.shift()!;
+        const pos = tmpArr[0];
+        const steps = tmpArr[1];
+        if (pos === end) return steps;
+        const res = [
+            lines[0].length + pos,
+            -lines[0].length + pos,
+            1 + pos,
+            -1 + pos,
+        ].filter((r) => terrain[r] <= terrain[pos] + 1 && !cache.has(r));
+        res.forEach((r) => cache.add(r));
+        queue = [...queue, ...res.map((c) => [c, steps + 1])];
+    }
+    return Infinity;
+};
+
+console.log(bfs(start));
+
+console.log(
+    terrain
+        .map((c, i) => ({ c, start: i }))
+        .filter(({ c }) => c === "a".charCodeAt(0))
+        .map(({ start }) => bfs(start))
+        .reduce((min, v) => (min < v ? min : v))
+);
